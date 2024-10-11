6d062a9cc751b214a1efb2ebc376be9beaca8a54
Mr_Robot <66792944+Warfarian@users.noreply.github.com>
2024-10-11 19:13:07 +0530

    basic styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+function displayOnload(){
+    let welcome = document.createElement('h1');
+    let wrapper = document.querySelector('.wrapper');
+    welcome.textContent = "WELCOME";
+    welcome.className = "welcome";
+    wrapper.appendChild(welcome);
+}
+
+window.onload(displayOnload());
